a619005bde7efec88e52d4dfab1bab2ab43bc853
ruiarroz <rui.octave@gmail.com>
2020-09-23 18:16:26 +0100

    1 file modified

--- app/views/home/home.component.ts
+++ app/views/home/home.component.ts
@@ -9,19 +9,15 @@ import { Http } from '@angular/http';
     <li>1. Gerar um componente de nome "solidbinary" e coloca-lo na pasta <b>"views"</b></li>
     <li>2. O componente deve ser composto por 3 ficheiros (ts) (html) (css) </li>
     <li>3. Adicionar ao componente um titulo "Entrevista SolidBinary" centrado horizontalmente de cor preta e fundo: #255255255; </li>
-    <li>3. Criar o routing "/entrevista" para o componente </li>
-    <li>4. Adicionar hiperligação "Entrevista" a seguir ao <b>"Home"</b> no topo da página</li>
+    <li>4. Usar o seguinte endereço <b>"https://jsonplaceholder.typicode.com/users"</b> para carregar uma lista de utilizadores em formato JSON</li>
+    <li>5. Exibir na visualização do componente a lista de todos os utlizadores mostrando unicamente o nome, o email e o username do utilizador.
+    <li>5. Criar o routing "/entrevista" para o componente </li>
+    <li>6. Adicionar hiperligação "Entrevista" a seguir ao <b>"Home"</b> no topo da página</li>
     </ul>
   `,
 })
 export class HomeViewComponent {
   users;
 
-  constructor(private http: Http) {
-    this.http.get('https://jsonplaceholder.typicode.com/users')
-      .map(res => res.json())
-      .subscribe(res => {
-        console.log(res);
-        this.users = res})
-  }
+
 }
