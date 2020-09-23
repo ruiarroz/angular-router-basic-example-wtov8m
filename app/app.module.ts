3efb21599b6f34115ac587d4773d1e16aec0d824
ruiarroz <rui.octave@gmail.com>
2020-09-23 18:12:52 +0100

    3 files modified

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
@@ -11,7 +12,7 @@ import { SolidbinaryComponent } from './views/solidbinary/solidbinary.component'
   declarations: [ AppComponent, SolidbinaryComponent],
   imports: [ 
     AppRoutingModule, 
-    BrowserModule, FormsModule, HttpModule 
+    BrowserModule, FormsModule, HttpModule ,CommonModule
   ],
   bootstrap: [ AppComponent ]
 })
--- app/views/solidbinary/solidbinary.component.ts
+++ app/views/solidbinary/solidbinary.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-solidbinary',
@@ -6,8 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./solidbinary.component.css']
 })
 export class SolidbinaryComponent implements OnInit {
-
-  constructor() { }
+users;
+   constructor(private http: Http) {
+    this.http.get('https://jsonplaceholder.typicode.com/users')
+      .map(res => res.json())
+      .subscribe(res => {      
+        this.users = res})
+  }
 
   ngOnInit() {
   }
